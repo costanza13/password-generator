c1e7df50717fab4c311078b456d9a7dd05005253
mcc <costanza+git@gmail.com>
2021-04-02 14:26:09 -0700

    add password lenth prompt, criteria selection code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,48 @@
 // Assignment code here
 function generatePassword() {
-  var length = getPasswordLength();
+  var password = '';
+  var pwLength = getPasswordLength();
   var criteria = getCriteria();
 
-  console.log('length ', length);
+  console.log('pwLength ', pwLength);
   console.log('criteria ', criteria);
 
-  return "password";
+  var currentType;
+  for (var i = 0; i < pwLength; i++) {
+    // randomly choose next character type from critera
+    currentType = criteria[randomNumber(0, criteria.length - 1)];
+    console.log(currentType);
+
+    // then randomly select a character of the chosen type
+    password += getRandomCharacter(currentType);
+    console.log('pw: ', password);
+  }
+
+  return password;
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
 
 function getCriteria() {
-  var criteria = {
-    uppercase: getSelection('lowercase'),
-    lowerrcase: getSelection('uppercase'),
-    numeric: getSelection('nnumeric'),
-    special: getSelection('special'),
-  };
+  var criteria = [];
+  
+  while (criteria.length === 0) {
+    if (window.confirm("Include lowercase letters?")) {
+      criteria.push('lowercase');
+    }
+    if (window.confirm("Include uppercase letters?")) {
+      criteria.push('uppercase');
+    }
+    if (window.confirm("Include numeric characters?")) {
+      criteria.push('numeric');
+    }
+    if (window.confirm("Include special characters?")) {
+      criteria.push('special');
+    }
+  }
+
   return criteria;
 }
 
@@ -23,15 +50,16 @@ function getPasswordLength() {
   var length = -1;
   
   while (isNaN(length) || length < 8 || length > 128) {
-    window.prompt("Please enter a length for your password between 8 and 128 characters.");
+    length = window.prompt("Please enter a length for your password between 8 and 128 characters.");
     length = parseInt(length);
+    console.log("length input: ", length);
   }
 
   return length;
 }
 
-function getSelection(characterType) {
-  return true;
+function getRandomCharacter(characterType) {
+  return "a";
 }
 
 // Get references to the #generate element
